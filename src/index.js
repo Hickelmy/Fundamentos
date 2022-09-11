cc8c9817d15132dd37d31f6b41426d5912fb255b
Riquelmy Costa <50642817+Hickelmy@users.noreply.github.com>
2022-09-11 11:02:15 -0400

    feat: importando arquivo css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import ReactDOM from "react-dom";
 import React from "react";
 import "./index.css";
-
 const tag = <strong>Ola REACT</strong>;
 ReactDOM.render(<div>{tag}</div>, document.getElementById("root"));
